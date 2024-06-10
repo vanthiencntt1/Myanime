1f8353ae818a0301c984820b44246b5dc0245bbe
VanThien <vanthien@gmail.com>
2024-06-10 22:35:34 +0700

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === "production" ? "" : "",
   publicRuntimeConfig: {
     apiPath: "http://mqsoft.ddns.net:9998",
-    contextPath: process.env.NODE_ENV === "production" ? "" : "",
-    uploadPath:
-      process.env.NODE_ENV === "production" ? "/upload.php" : "/api/upload",
   },
 };
 
